432d9aa5f1e4a1438aea43227a149362a03fa680
Mac <mac@MacBook-Pro-Mac-2.local>
2022-06-28 19:10:52 +0200

    design changes + api

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-const xhr = new XMLHttpRequest();
-console.log(xhr);
+function fetchWeather(city) {
+    const apiKey = "09c5c2dfec3b4287df8859c27e18db3c";
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apiKey) 
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+}
